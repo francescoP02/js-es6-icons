7d958f56a268beb65f020ea5c39a86ca7cf56434
francescoP02 <file.partipilo@gmail.com>
2022-05-02 16:26:06 +0200

    MILESTONE 1 done

--- js/data.js
+++ js/data.js
@@ -1,4 +1,16 @@
-[
+// Milestone 1
+// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+// Milestone 2
+// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+// :muso_unicorno: BONUS
+// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+// :lampadina: Consigli del giorno
+// Come sempre, iniziamo prima di tutto dall'analisi e comprensione della consegna. Scomponiamo il problema in micro-passaggi logici che solamente in un secondo momento trasformeremo in codice.
+// Le icone presenti nella struttura dati fanno riferimento alla nota libreria Font Awesome (versione 5), perciò come prima cosa assicuriamoci di aver inserito il link alla cdn nell'head della pagina.
+// Dopodiché, basandoci sul codice di riferimento nel sito di Font Awesome, analizziamo come è formato il tag <i> di un'icona qualsiasi, in particolare focalizziamoci sulle classi.
+// Come possiamo usare i dati presenti nella nostra struttura dati per creare l'elemento html nel modo corretto e visualizzare l'icona in pagina?
+
+const icons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +124,11 @@
 		color: 'blue'
 	}
 ];
+
+icons.forEach((element) =>{
+	const div = document.querySelector("div.container");
+	const iconFamily = element.family;
+	const iconPrefix = element.prefix;
+	const iconName = element.name;
+	div.innerHTML += `<div class="card"><span><i class="${iconFamily} ${iconPrefix}${iconName}"></i></span></div>`;
+});
